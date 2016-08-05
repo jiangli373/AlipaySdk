4e1b1df5535ec14cf1f128d9b0fe7660401a5eb2
jiangli <liminghaoze@qq.com>
2016-08-05 10:36:37 +0800

    支付宝手机支付

--- controller/AlipayController.js
+++ controller/AlipayController.js
@@ -44,7 +44,7 @@ alipay.on('verify_fail', function(res){
 exports.create_direct_pay_by_user = function(req, res){
 	var method = req.method.toLowerCase();
 	if(method == 'get'){
-		res.render('create_direct_pay_by_user');
+		res.render('index');
 	}
 	else if(method == 'post'){
 
@@ -77,4 +77,34 @@ exports.user_auth_quick_login = function(req, res){
 	};
 
 	alipay.user_auth_quick_login(data, res);
+};
+
+
+exports.alipay_wap_create_direct_pay_by_user = function(req, res){
+	var method = req.method.toLowerCase();
+	if(method == 'get'){
+		res.render('wappay');
+	}
+	else if(method == 'post'){
+
+		//页面需要传递
+		/**
+		 * out_trade_no 商户网站唯一订单号   不能为空
+		 * subject 商品的标题/交易标题/订单标题/订单关键字等  该参数最长为128个汉字    不能为空
+		 * total_fee 交易金额 单位为RMB-Yuan。取值范围为[0.01，100000000.00]，精确到小数点后两位   不能为空
+		 * show_url  商品展示网址   可以为空
+		 * body 商品描述  对一笔交易的具体描述信息 可以为空
+		 * @type {{out_trade_no: *, subject: *, total_fee: *, body: *, show_url: *}}
+		 */
+
+		var data = {
+			out_trade_no:req.body.WIDout_trade_no
+			,subject:req.body.WIDsubject
+			,total_fee:req.body.WIDtotal_fee
+			,body: req.body.WIDbody
+			,show_url:req.body.WIDshow_url
+		};
+
+		alipay.alipay_wap_create_direct_pay_by_user(data, res);
+	}
 };
--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,14 @@ router.get('/login', function(req, res) {
 	res.render('login', { title: 'Express' });
 });
 
+router.get('/wappay', function(req, res) {
+	res.render('wappay', { title: 'Express' });
+});
+
+
 router.post('/create_direct_pay_by_user',AlipayController.create_direct_pay_by_user);
 router.post('/user_auth_quick_login',AlipayController.user_auth_quick_login);
-
+router.post('/alipay_wap_create_direct_pay_by_user',AlipayController.alipay_wap_create_direct_pay_by_user);
 
 
 module.exports = router;
